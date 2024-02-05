137d912945c367acf39dfa1b40e1f7823b556559
Davor Katona Šesto <davor.katona@gmail.com>
2024-02-05 07:27:38 +0100

    Add pure scss page

--- src/pages/scss-modules.js
+++ src/pages/scss-modules.js
@@ -9,24 +9,24 @@ export default function Page() {
 
                 <article className={styles.article}>
 
-                    <header className="article__header">
+                    <header className={styles.article__header}>
 
-                        <h1 className="article__title">
+                        <h1 className={styles.article__title}>
                             Exploring the Utility-First Paradigm in Tailwind CSS using @apply
                         </h1>
 
                         <img
-                            className="article__image"
+                            className={styles.article__image}
                             src="https://via.placeholder.com/1000x600"
                             alt="Tailwind CSS Image"
                         />
                     </header>
 
-                    <section className="article__description">
+                    <section className={styles.article__description}>
                         Discover the flexibility and efficiency of using Tailwind CSS, a highly customizable utility-first CSS framework that allows you to build designs directly in your markup.
                     </section>
 
-                    <section className="article__content">
+                    <section className={styles.article__content}>
                         <h2>
                             Introduction to Tailwind
                         </h2>
@@ -38,17 +38,17 @@ export default function Page() {
                         </p>
                     </section>
 
-                    <footer className="article__footer">
-                        <small className="article__footer_info">
+                    <footer className={styles.article__footer}>
+                        <small className={styles.article__footer_info}>
                             Published on 02. 02. 2024.
                         </small>
                     </footer>
                 </article>
 
-                <aside className="article__aside">
-                    <div className="article__aside_content">
+                <aside className={styles.article__aside}>
+                    <div className={styles.article__aside_content}>
                         <img
-                            className="article__aside_content_image"
+                            className={styles.article__aside_content_image}
                             src="https://via.placeholder.com/300x600"
                             alt="Banner"
                         />
